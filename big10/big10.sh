7b1b2b20d59cfdad1b2f58ec2fa366704f150a45
PartySlayer <squadrittichristian@gmail.com>
2023-08-03 16:02:32 +0000

    changes to make it faster

--- big10/big10.sh
+++ big10/big10.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 path=${1-/}
-err=$(find "$path" -type f 2>&1|grep "Permission denied")
 
-big10=$(find "$path" -type f 2>&1|grep -v "Permission denied"|du -h|sort -h|head -n 10)
+findbig() {
 
-if [ ! -z "$err" ]; then
-   echo "Retry with root privilegies"
-else
- echo  $big10>"$path+/big10.txt" && echo "the 10 biggest files were stored in big10.txt"
+ find $path -type f -exec du -h {} + 2>/dev/null|sort -rh|head -n 10 >"big10.txt"
+}
+
+findbig "$path"
+
+ echo "the 10 biggest files in $path were stored in big10.txt"
