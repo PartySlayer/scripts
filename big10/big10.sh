ec10c8e60b07769a8cca06c611a16c491d34dea7
PartySlayer <squadrittichristian@gmail.com>
2023-08-03 05:00:27 +0000

    small fixes and improvings

--- big10/big10.sh
+++ big10/big10.sh
@@ -7,8 +7,5 @@ big10=$(find "$path" -type f 2>&1|grep -v "Permission denied"|du -h|sort -h|head
 
 if [ ! -z "$err" ]; then
    echo "Retry with root privilegies"
-elif [ -e "$path+/big10.txt" ]; then
- echo  $big10>>"$path+/big10.txt" && echo "the 10 biggest files were stored in big10.txt"
 else
-   touch "$path/big10.txt" 2>&1 && echo $big10>>"$path/big10.txt" && echo "the 10 biggest files were stored in big10.txt"
-fi
+ echo  $big10>"$path+/big10.txt" && echo "the 10 biggest files were stored in big10.txt"
